90e46db1d1300d1b1bcfddb3e979d8783b75687d
Arzhan Kinzhalin <arzhan@kinzhalin.com>
2016-11-18 23:42:59 -0800

    Start work on mbox parsing.

---
+++ config.h
@@ -0,0 +1,8 @@
+#ifndef _MBOX_RDR_CONFIG_H
+#   define _MBOX_RDR_CONFIG_H   1
+
+#   define      DEBUG    1
+
+#   define      BUF_SIZE    2048
+
+#endif /* _MBOX_RDR_CONFIG_H */
--- mbox_rdr.c
+++ mbox_rdr.c
@@ -8,11 +8,19 @@
 #include <unistd.h>
 #include <assert.h>
 
+#include "config.h"
 #include "mbox_rdr.h"
+
+typedef enum state {
+    MSG_START_MAYBE,
+    MSG_START_MUST,
+    MSG_HDR,
+    MSG_BODY
+} state_t;
 
 static void print_str(const char *buf, const unsigned int len) {
     /* this is debug only */
-    char str[2048];
+    char str[BUF_SIZE];
     memcpy(str, buf, len);
     *(str+len) = '\0';
     fprintf(stdout, "%s\n", str);
@@ -31,9 +39,51 @@ static int _get_next_line(const char *buf, unsigned int size, unsigned int *len)
     }
 }
 
+static unsigned char _is_from_line(const char *line, unsigned int len) {
+    return !strncmp(line, "From ", 5) ? 1 : 0;
+}
+
+static int _parse_line(const char *line, unsigned int len) {
+    static state_t st = MSG_START_MUST;
+    switch (st) {
+        case MSG_START_MUST:
+        case MSG_START_MAYBE:
+            if (!len) {
+                break;
+            } else if (len < 6) { /* F, R, O, M, space, and at least one more character */
+                if (st == MSG_START_MAYBE) st = MSG_BODY;
+                return st == MSG_START_MUST ? 1 : 0;
+            } else {
+                if (_is_from_line(line, len)) {
+#                           ifdef DEBUG
+                    fprintf(stdout, "Found message.\n");
+#                           endif
+                    st = MSG_HDR;
+                } else {
+                    if (st == MSG_START_MAYBE) st = MSG_BODY;
+                    return st == MSG_START_MUST ? 1 : 0;
+                }
+            }
+            break;
+        case MSG_HDR:
+            if (!len) {
+                st = MSG_BODY;
+            }
+            break;
+        case MSG_BODY:
+            if (!len) {
+                st = MSG_START_MAYBE;
+            }
+            break;
+    }
+    return 0;
+}
+
 static int _mboxr_parse(const char *name, mbox_info_t *info) {
-    char buf[2048];
+    char buf[BUF_SIZE];
+#   ifdef DEBUG
     unsigned int lc = 0;
+#   endif
     int fd, bytes;
     unsigned int off, len, avail;
 
@@ -44,27 +94,35 @@ static int _mboxr_parse(const char *name, mbox_info_t *info) {
     off = 0;
     len = 0;
     /* read the chunk */
-    while ((bytes = read(fd, buf+len, 2048-len)) != -1) {
+    while ((bytes = read(fd, buf+len, BUF_SIZE-len)) != -1) {
         avail = len + bytes;
         while (1) {
             /* find the next line */
             if (_get_next_line((const char *)buf+off, avail-off, &len) == -1) {
-                // copy the leftover to the beginning and retry
-                char tmp[2048];
+                /* copy the leftover to the beginning and retry */
+                char tmp[BUF_SIZE];
                 assert(off+len == avail);
                 memcpy(tmp, buf+off, len);
                 memcpy(buf, tmp, len);
                 off = 0;
                 break;
             }
-            lc++;
+
+            _parse_line((const char *)buf+off, len);
+
             off += len+1;
             len = 0;
+#           ifdef DEBUG
+            lc++;
+#           endif
         }
         if (!bytes) {
             break;
         }
     }
+#   ifdef DEBUG
+    fprintf(stdout, "Total number of lines: %d.\n", lc);
+#   endif
     return 0;
 }
 
